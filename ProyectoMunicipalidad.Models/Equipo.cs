dea5e43ba547e71531d4b403f9e5df101567daaf
Checho2200 <sergiociudadora2207@gmail.com>
2025-05-10 18:56:28 -0500

    Add files via upload

---
+++ ProyectoMunicipalidad.Models/Equipo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoMunicipalidad.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre o descripcion del equipo es obligatoria.")]
+        [StringLength(250)]
+        public string Nombre { get; set; } // Ej: "Impresora láser multifunción HP"
+
+
+        [StringLength(100)]
+        public string? Marca { get; set; }
+
+        [StringLength(100)]
+        public string? Modelo { get; set; }
+
+        [Required(ErrorMessage = "Fecha de registro en area de informatica")]
+        [DataType(DataType.Date)]
+        public DateTime FechaAdquisicion { get; set; }
+
+        [Required(ErrorMessage = "El estado del equipo es obligatorio.")]
+        [StringLength(50)]
+        public string Estado { get; set; } // Ej: "Operativo", "En mantenimiento", "Fuera de servicio"
+
+        [Required(ErrorMessage = "La ubicación del equipo es obligatoria.")]
+        [StringLength(100)]
+        public string Ubicacion { get; set; } // Ej: "Oficina de Finanzas - Segundo Piso"
+
+        [Required(ErrorMessage = "Registrar el encargado de la maquina")]
+        [StringLength(100)]
+        public string Responsable { get; set; } // Persona encargada o usuaria del equipo
+
+        //relacion con el mantenimiento
+        public ICollection<Mantenimiento>? Mantenimientos { get; set; }
+    }
+}
---
+++ ProyectoMunicipalidad.Models/Mantenimiento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoMunicipalidad.Models
+{
+    public class Mantenimiento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La fecha de mantenimiento es obligatoria")]
+        [Display(Name = "Fecha de Mantenimiento")]
+        [DataType(DataType.Date)]
+        public DateTime FechaMantenimiento { get; set; }
+
+        [Required(ErrorMessage = "El tipo de mantenimiento es obligatorio")]
+        [Display(Name = "Tipo de Mantenimiento")]
+        public string TipoMantenimiento { get; set; }
+
+        [Required(ErrorMessage = "El estado es obligatorio")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "El responsable es obligatorio")]
+        public string Responsable { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        public string Descripcion { get; set; }
+
+        // Relación con Equipo
+        [Required(ErrorMessage = "Debe seleccionar un equipo")]
+        public int EquipoId { get; set; }
+
+        [ForeignKey("EquipoId")]
+        public Equipo Equipo { get; set; }
+    }
+}
